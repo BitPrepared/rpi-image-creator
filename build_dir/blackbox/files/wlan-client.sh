b3c2c6f30c8fbc1f995fbc3f778b185a8ac02544
Stefano Tamagnini <yoghi@sigmalab.net>
2026-08-25 03:30:23 +0200

    fix

--- build_dir/blackbox/files/wlan-client.sh
+++ build_dir/blackbox/files/wlan-client.sh
@@ -1,3 +1,3 @@
 ifconfig wlan0 0.0.0.0 down
-wpa_supplicant -B -i wlan0 -c /root/wpa-bitprepared.conf
+wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 ifconfig wlan0 172.16.6.2 netmask 255.255.255.0
