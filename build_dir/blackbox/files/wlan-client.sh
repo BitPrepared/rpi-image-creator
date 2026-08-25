d3ec4781a822bb18c492a0c2be0225c19be3d65b
Daniele Palumbo <daniele@retaggio.net>
2026-08-25 04:39:33 +0200

    added default gw for the manual wlan config

--- build_dir/blackbox/files/wlan-client.sh
+++ build_dir/blackbox/files/wlan-client.sh
@@ -1,3 +1,4 @@
 ifconfig wlan0 0.0.0.0 down
 wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
 ifconfig wlan0 172.16.6.2 netmask 255.255.255.0
+route add default gw 172.16.6.254
